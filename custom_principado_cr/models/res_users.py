d663ff1e6a3c73b229f712752fb303c3e9e260b4
hardik <hardik@candidroot.com>
2022-11-11 17:51:29 +0530

    [IMP] add changes in rule show pos and invoice

--- custom_principado_cr/models/res_users.py
+++ custom_principado_cr/models/res_users.py
@@ -7,6 +7,17 @@ class ResUsers(models.Model):
     _inherit = 'res.users'
 
     warehouse_ids = fields.Many2many("stock.warehouse", string="Warehouse")
+    pos_config_ids = fields.Many2many(
+        comodel_name='pos.config',
+        string='Allowed POS',
+        help="Allowed Points of Sales for the user. "
+             "POS managers can use all POS.",
+    )
+    journal_ids = fields.Many2many(
+        comodel_name='account.journal',
+        string='Allowed Journal',
+        help="Allowed Journal "
+    )
 
     @api.model
     def create(self, vals):
@@ -16,6 +27,10 @@ class ResUsers(models.Model):
 
     def write(self, vals):
         # Clear the cache in order to recompute _get_active_rules
+        if self.ids and 'pos_config_ids' in vals:
+            self.env['ir.rule'].clear_caches()
+        if self.ids and 'journal_ids' in vals:
+            self.env['ir.rule'].clear_caches()
         self.clear_caches()
         return super(ResUsers, self).write(vals)
 
