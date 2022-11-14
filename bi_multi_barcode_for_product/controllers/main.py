0cfd767a9fe98c22e00a1d8264220ba7c842b032
hardik <hardik@candidroot.com>
2022-11-14 18:06:40 +0530

    add missing barcode products

--- bi_multi_barcode_for_product/controllers/main.py
+++ bi_multi_barcode_for_product/controllers/main.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import http
+from collections import defaultdict
+from odoo import http, _
 from odoo.http import request
+from odoo.osv import expression
 from odoo.addons.stock_barcode.controllers.stock_barcode import StockBarcodeController
-
 
 class StockBarcodeProduct(StockBarcodeController):
 
@@ -36,3 +37,39 @@ class StockBarcodeProduct(StockBarcodeController):
                     },
                 }
             }
+
+    @http.route('/stock_barcode/get_specific_barcode_data', type='json', auth='user')
+    def get_specific_barcode_data(self, barcode, model_name, domains_by_model=False):
+        nomenclature = request.env.company.nomenclature_id
+        # Adapts the search parameters for GS1 specifications.
+        operator = '='
+        limit = None if nomenclature.is_gs1_nomenclature else 1
+        if nomenclature.is_gs1_nomenclature:
+            try:
+                # If barcode is digits only, cut off the padding to keep the original barcode only.
+                barcode = str(int(barcode))
+                operator = 'ilike'
+            except ValueError:
+                pass  # Barcode isn't digits only.
+
+        domains_by_model = domains_by_model or {}
+        barcode_field_by_model = self._get_barcode_field_by_model()
+        result = defaultdict(list)
+        model_names = model_name and [model_name] or list(barcode_field_by_model.keys())
+        for model in model_names:
+            domain = [(barcode_field_by_model[model], operator, barcode)]
+            domain_for_this_model = domains_by_model.get(model)
+            if model == 'product.product':
+                domain = expression.OR([domain,
+                                        [('product_barcode.barcode', operator, barcode)]
+                                        ])
+            if domain_for_this_model:
+                domain = expression.AND([domain, domain_for_this_model])
+            record = request.env[model].with_context(display_default_code=False).search(domain, limit=limit)
+            if record:
+                result[model] += record.read(request.env[model]._get_fields_stock_barcode(), load=False)
+                if hasattr(record, '_get_stock_barcode_specific_data'):
+                    additional_result = record._get_stock_barcode_specific_data()
+                    for key in additional_result:
+                        result[key] += additional_result[key]
+        return result
