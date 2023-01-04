9562908437d179205e87e8b3bfe4eb59dd9a15de
erp <erp>
2023-01-04 20:36:08 +0530

    [IMP] Fix issue

--- import_product_excel_cr/duplicate_product_template_script_optimized.py
+++ import_product_excel_cr/duplicate_product_template_script_optimized.py
@@ -29,21 +29,19 @@ with open('/home/odoo/src/user/import_product_excel_cr/principado_product/final_
 
         name_read = sock.execute_kw(dbname, 2, password,
                           'product.product', 'search_read',
-                          [[
-                                            ('size_attribute_value_id', 'in', [size_value_id[0]] ),
-                                            ('color_attribute_value_id', 'in', [color_value_id[0]])
-                                            ]],
+                          [[('size_attribute_value_id', 'in', [size_value_id[0]] ),('color_attribute_value_id', 'in', [color_value_id[0]])]],
                           {'fields': ['product_tmpl_id', 'name']})
-        _logger.info('------------>>>>>>>>> product search read ---- %s',name_read)
-        print ('------------>>>>>>>>> product search read ----', name_read)
-        if name_read and name_read[0]['name'] == line[1] + ' -Duplicate':
-            barcode_id = sock.execute(dbname, 2, password, 'product.barcode', "search", [('barcode', '=', line[2] + '-Duplicate')])
-            barcode_update = sock.execute(dbname, 2, password, 'product.barcode', "write",
-                                          [barcode_id[0]],
-                                          {"product_id": name_read[0]['id'],
-                                           'product_tmpl_id': name_read[0]['product_tmpl_id'][0]})
-            product_variant_update = sock.execute(dbname, 2, password, 'product.product', "write",
-                                                  [name_read[0]['id']], {"default_code": line[3]})
-        _logger.info('line number ------------------->>>>%s', line[0])
-        print ('===================',line[0])
+
+        for r in name_read:
+            if r and r['name'] == line[1] + ' -Duplicate':
+                barcode_id = sock.execute(dbname, 2, password, 'product.barcode', "search", [('barcode', '=', line[2] + '-Duplicate')])
+                print('------------barcode_id----', barcode_id)
+                barcode_update = sock.execute(dbname, 2, password, 'product.barcode', "write",
+                                              [barcode_id[0]],
+                                              {"product_id": r['id'],
+                                               'product_tmpl_id': r['product_tmpl_id'][0]})
+                product_variant_update = sock.execute(dbname, 2, password, 'product.product', "write",
+                                                      [r['id']], {"default_code": line[3]})
+                print('------------write---', product_variant_update)
+            print ('===================',line[0])
 
