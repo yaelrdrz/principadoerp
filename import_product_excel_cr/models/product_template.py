7af43f31973a563198cfb3eb438e838ecf087689
ERP <ERP>
2023-01-04 14:17:52 +0530

    Create variant ids

--- import_product_excel_cr/models/product_template.py
+++ import_product_excel_cr/models/product_template.py
@@ -612,6 +612,82 @@ class ProductTemplate(models.Model):
                 return [resp[0].get("id")]
         return False
 
+    def _create_variant_ids(self):
+        self.flush()
+        Product = self.env["product.product"]
+
+        variants_to_create = []
+        variants_to_activate = Product
+        variants_to_unlink = Product
+
+        for tmpl_id in self:
+            lines_without_no_variants = tmpl_id.valid_product_template_attribute_line_ids._without_no_variant_attributes()
+
+            all_variants = tmpl_id.with_context(active_test=False).product_variant_ids.sorted(lambda p: (p.active, -p.id))
+
+            current_variants_to_create = []
+            current_variants_to_activate = Product
+
+            single_value_lines = lines_without_no_variants.filtered(lambda ptal: len(ptal.product_template_value_ids._only_active()) == 1)
+            if single_value_lines:
+                for variant in all_variants:
+                    combination = variant.product_template_attribute_value_ids | single_value_lines.product_template_value_ids._only_active()
+                    # Do not add single value if the resulting combination would
+                    # be invalid anyway.
+                    if (
+                        len(combination) == len(lines_without_no_variants) and
+                        combination.attribute_line_id == lines_without_no_variants
+                    ):
+                        variant.product_template_attribute_value_ids = combination
+
+            # Set containing existing `product.template.attribute.value` combination
+            existing_variants = {
+                variant.product_template_attribute_value_ids: variant for variant in all_variants
+            }
+
+            if not tmpl_id.has_dynamic_attributes():
+                all_combinations = itertools.product(*[
+                    ptal.product_template_value_ids._only_active() for ptal in lines_without_no_variants
+                ])
+                # For each possible variant, create if it doesn't exist yet.
+                for combination_tuple in all_combinations:
+                    combination = self.env['product.template.attribute.value'].concat(*combination_tuple)
+                    is_combination_possible = tmpl_id._is_combination_possible_by_config(combination, ignore_no_variant=True)
+                    if not is_combination_possible:
+                        continue
+                    if combination in existing_variants:
+                        current_variants_to_activate += existing_variants[combination]
+                    else:
+                        current_variants_to_create.append(tmpl_id._prepare_variant_values(combination))
+                        # if len(current_variants_to_create) > 1000:
+                        #     raise UserError(_(
+                        #         'The number of variants to generate is too high. '
+                        #         'You should either not generate variants for each combination or generate them on demand from the sales order. '
+                        #         'To do so, open the form view of attributes and change the mode of *Create Variants*.'))
+                variants_to_create += current_variants_to_create
+                variants_to_activate += current_variants_to_activate
+
+            else:
+                for variant in existing_variants.values():
+                    is_combination_possible = self._is_combination_possible_by_config(
+                        combination=variant.product_template_attribute_value_ids,
+                        ignore_no_variant=True,
+                    )
+                    if is_combination_possible:
+                        current_variants_to_activate += variant
+                variants_to_activate += current_variants_to_activate
+
+            variants_to_unlink += all_variants - current_variants_to_activate
+
+        if variants_to_activate:
+            variants_to_activate.write({'active': True})
+        if variants_to_create:
+            Product.create(variants_to_create)
+        if variants_to_unlink:
+            variants_to_unlink._unlink_or_archive()
+        self.flush()
+        self.invalidate_cache()
+        return True
 
     # def _create_variant_ids(self):
     #     self.flush()
